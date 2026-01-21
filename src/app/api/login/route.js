410f5b67ae01cbc1851cbc79a0c773606288a18e
Satyaprakash8543 <prajapatisatya8543@gmail.com>
2026-01-21 16:53:06 +0530

    add loginapi& login form in browser to code base!

---
+++ src/app/api/login/route.js
@@ -0,0 +1,61 @@
+import { NextResponse } from "next/server";
+import { User } from "../../../models/user";
+ import { connectDb } from "../../../helper/db";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+
+connectDb();
+export async function POST(request) {
+  const { email, password } = await request.json(); //get user(email&password)
+
+  try {
+    // 1. get user 
+    const user = await User.findOne({
+      email:email,
+    });
+     
+    if(user == null){
+        throw new Error("User not found!");
+    }
+    //2- password check  
+   const matched= bcrypt.compareSync(password,user.password)
+   if(!matched){
+    throw new Error("Password not matched!!");
+   }
+   //3 generate token
+    const token = jwt.sign({
+    _id:user._id,
+    name:user.name
+   },process.env.JWT_KEY)
+
+   //4 create nextresponce cookie
+       const response=NextResponse.json({
+        message:"Login success !!",
+        success:true
+       })
+     //5 set token into cookies
+       response.cookies.set("authToken",token,{
+        expiresIn:"1d",
+        httpOnly:true
+       })
+
+    console.log( "user data check="+user);
+    console.log("Token generate verify="+ token);
+    // return NextResponse.json({ //hide 5 step cookies set up
+    //   message: "success",
+    // });
+    return response;
+
+
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+        success: false,
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -1,104 +1,122 @@
-"use client"
-import React, { useState } from 'react';
-import { toast } from 'react-toastify';
+"use client";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import {login } from "../../services/userServices"
+import { useRouter } from "next/navigation";
+
 
 const Login = () => {
+  const router=useRouter();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
 
-   const [loginData, setLoginData]= useState({
-        email:"",
-        password:""
-    })
-
-    const loginFormSubmitted=(event)=>{
-         event.preventDefault();
-        //  console.log(event);
-        console.log(loginData);
-        if(loginData.email.trim() === "" || loginData.password.trim() === ""){
-            toast.info("Invalid data!!",{
-                position:"top-center",
-            })
-            return;
-        }
-
-        // Validate data
-        //login kro
+  const loginFormSubmitted = async (event) => {
+    event.preventDefault();
+    //  console.log(event);
+    console.log(loginData);
+    if (loginData.email.trim() === "" || loginData.password.trim() === "") {
+      toast.info("Invalid data!!", {
+        position: "top-center",
+      });
+      return;
     }
+
+    // Validate data
+    //login (api)
+
+    try {
+      const result = await login(loginData)
+      console.log(result);
+      toast.success("Logged In");
+      //After login redirect page
+      router.push("/profile/user")
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message, {
+        position: "top-center",
+      });
+    }
+  };
   return (
     <div className="container grid grid-cols-12 ">
       <div className="col-span-5 col-start-5  p-4">
-        <div className='py-2'></div>
+        <div className="py-2"></div>
 
-        <h1 className="text-2xl text-center hover:text-teal-500">
-            Login Here
-          </h1>
-          <form action="#!" onSubmit={loginFormSubmitted}>
-                    {/* email */}
-            <div className="mt-2">
-              <label
-                className="block text-xl font-medium mb-2 ps-3"
-                htmlFor="user_email"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                className="w-full p-2.5 rounded-2xl bg-gray-600 focus:ring-gray-100 border border-gray-500"
-                placeholder="Enter email"
-                id="user_email"
-                name="user_email"
-                onChange={(event) => {
-                  setLoginData({
-                    ...loginData,
-                    email: event.target.value,
-                  });
-                }}
-                value={loginData.email}
-              />
-            </div>
+        <h1 className="text-2xl text-center hover:text-teal-500">Login Here</h1>
+        <form action="#!" onSubmit={loginFormSubmitted}>
+          {/* email */}
+          <div className="mt-2">
+            <label
+              className="block text-xl font-medium mb-2 ps-3"
+              htmlFor="user_email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              className="w-full p-2.5 rounded-2xl bg-gray-600 focus:ring-gray-100 border border-gray-500"
+              placeholder="Enter email"
+              id="user_email"
+              name="user_email"
+              onChange={(event) => {
+                setLoginData({
+                  ...loginData,
+                  email: event.target.value,
+                });
+              }}
+              value={loginData.email}
+            />
+          </div>
 
-             {/* password */}
-            <div className="mt-2">
-              <label
-                className="block text-xl font-medium mb-2 ps-3"
-                htmlFor="user_password"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                className="w-full p-2.5 rounded-2xl bg-gray-600 focus:ring-gray-100 border border-gray-500"
-                placeholder="Enter password"
-                id="user_password"
-                name="user_password"
-                onChange={(event) => {
-                  setLoginData({
-                    ...loginData,
-                    password: event.target.value,
-                  });
-                }}
-                value={loginData.password}
-              />
-            </div>
+          {/* password */}
+          <div className="mt-2">
+            <label
+              className="block text-xl font-medium mb-2 ps-3"
+              htmlFor="user_password"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              className="w-full p-2.5 rounded-2xl bg-gray-600 focus:ring-gray-100 border border-gray-500"
+              placeholder="Enter password"
+              id="user_password"
+              name="user_password"
+              onChange={(event) => {
+                setLoginData({
+                  ...loginData,
+                  password: event.target.value,
+                });
+              }}
+              value={loginData.password}
+            />
+          </div>
 
-              {/* Button field  */}
-              <div className="mt-2 text-center">
-              <button
-                type="submit"
-                className="px-3 py-2 bg-green-600 rounded-xl hover:bg-green-400"
-              >
-                Login
-              </button>
-              <button  type="button" className="px-3 py-2 bg-orange-600 rounded-xl hover:bg-orange-400 mx-2">
-                Reset
-              </button>
-            </div>
-              
-            {/* check the change value is verify */}
-            {/* {JSON.stringify(loginData)} */}
-          </form>
-        </div>
-        </div>
+          {/* Button field  */}
+          <div className="mt-2 text-center">
+            <button
+              type="submit"
+              className="px-3 py-2 bg-green-600 rounded-xl hover:bg-green-400"
+            >
+              Login
+            </button>
+            <button
+              type="button"
+              className="px-3 py-2 bg-orange-600 rounded-xl hover:bg-orange-400 mx-2"
+            >
+              Reset
+            </button>
+          </div>
+
+          {/* check the change value is verify */}
+          {/* {JSON.stringify(loginData)} */}
+        </form>
+      </div>
+    </div>
   );
-}
+};
 
 export default Login;
